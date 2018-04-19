7997df7463d7a3e55b4635029339d3890dccaa43
Andy_Weng <Andy_Weng@wistron.com>
2018-04-19 15:22:02 +0800

    update integration

--- week3_Integration/.eslintrc.js
+++ week3_Integration/.eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
         "no-debugger": [0],
         "import/prefer-default-export": [0],
         "class-methods-use-this": [0],
-        "no-new": [0]
+        "no-new": [0],
+        "max-len": [0]
     }
 };
